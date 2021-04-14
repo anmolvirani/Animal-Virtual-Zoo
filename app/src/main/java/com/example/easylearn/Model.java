d6b146a2dee807cc3abc6626a6fd6c0ef94fcfe7
naveen-gthb <11699nk@gmail.com>
2021-04-14 20:44:31 +0530

    Fixing UI and setting pitch = 1/4 and speed = 1/8

--- app/src/main/java/com/example/easylearn/Model.java
+++ app/src/main/java/com/example/easylearn/Model.java
@@ -269,8 +269,8 @@ public class Model extends AppCompatActivity {
     public void speakTiger()
     {
         String text = "The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange-brown fur with a lighter underside.A punch from a Tiger may kill you.Tiger cubs are born blind and only half of the cubs survive. ";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -282,8 +282,8 @@ public class Model extends AppCompatActivity {
     public void speakLion()
     {
         String text = "The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail.Lion’s roar can be heard from 8kms away.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -311,8 +311,8 @@ public class Model extends AppCompatActivity {
     public void speakJag()
     {
         String text = "The jaguar is the third biggest cat in the world - after the tiger and the lion - and is the largest cat in the Americas.The jaguar has a very powerful jaw, its bite exerts more force than that of a lion.Jaguars live in the wild for 11-15 years, in captivity they can live over 20 years.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -355,8 +355,8 @@ public class Model extends AppCompatActivity {
     public void speakCheetah()
             {
                 String text = "The cheetah is the world's fastest land mammal. With acceleration that would leave most automobiles in the dust, a cheetah can go from 0 to 60 miles an hour in only three seconds.They're Built for Speed.Cheetahs Don't Roar, They Meow and Purr.They're Racing Toward Extinction.";
-                float pitch = 1/2;
-                float speed = 1/2;
+                float pitch = 1/4;
+                float speed = 1/8;
 
                 mTTS.setPitch(pitch);
                 mTTS.setSpeechRate(speed);
@@ -399,8 +399,8 @@ public class Model extends AppCompatActivity {
     public void speakMule()
     {
         String text = "Mule Deer are the largest member of the Odocoileus genus. They gained their name from their large, mule like ears which they can move independently, allowing them to survey their surroundings for sounds of potential danger. Mule deer stand 40 to 42 inches tall at the shoulders and weigh an average of 200 pounds.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -444,8 +444,8 @@ public class Model extends AppCompatActivity {
     public void speakSnow()
     {
         String text = "Snow leopards have powerful legs and are tremendous jumpers, able to leap as far as 50 feet. These big cats use their long tails for balance and as blankets to cover sensitive body parts against the severe mountain chill. They are shy and reclusive, and rarely seen in the wild.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -488,8 +488,8 @@ public class Model extends AppCompatActivity {
     public void speakZebra()
     {
         String text = "Zebras are single-hoofed animals that are native to Africa. Zebras are very closely related to horses and donkeys; in fact, they are in the same genus, Equus. ... The Grevy's zebra has very thin stripes.Every zebra has a unique pattern of black and white stripes.There are a number of different theories which attempt to explain zebra's unique stripes with most relating to camouflage.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
@@ -533,8 +533,8 @@ public class Model extends AppCompatActivity {
     public void speakSnake()
     {
         String text = "There are more than 3,000 species of snakes on the planet and they're found everywhere except in Antarctica, Iceland, Ireland, Greenland, and New Zealand. Snakes are Deaf and Cannot Hear Sounds.Most Poisonous Snakes Have Bright Colours.Snakes Live Longer in Zoos Than in the Wild.";
-        float pitch = 1/2;
-        float speed = 1/2;
+        float pitch = 1/4;
+        float speed = 1/8;
 
         mTTS.setPitch(pitch);
         mTTS.setSpeechRate(speed);
